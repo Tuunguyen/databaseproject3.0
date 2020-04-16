7bbdfb7ad6a6d9b62d6ef0154ebdea1097c73a12
Tuu Nguyen <tuu.nguyen@live.com>
2020-04-15 21:17:12 -0500

    please work

---
+++ public/js/setup.js
@@ -0,0 +1,331 @@
+var checkLogIn = function () {
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	$.getJSON('/' + id +'/profile.json', function(js) { //return json of employee tuple from their id 
+	  $('#id').attr("id", js.data[0].empID);
+	  $('#fName').html(js.data[0].firstName);
+	  $('#lName').html(js.data[0].lastName);
+	  if(js.data[0].empType == "admin"){
+	  }
+	  else if(js.data[0].empType == "pl"){
+		$( ".admin" ).remove();
+		$( ".pl" ).remove();
+	  }
+	  else{
+		$( ".admin" ).remove();
+		$( ".pl" ).remove();
+	  }
+   })
+	  .done(function() {
+		  $.getJSON('/' + id +'/homeLoad.json', function(js) { //return json of array of project tuples where employee id is in employee id list
+			  if(js.data != null){
+				  $("#ifNoProj").remove();
+			  }
+			  console.log(js.data);
+			  $.each( js.data, function( id, project){
+				var m = (project.finDate).split('-');
+				$("#projects").append(
+				'<div class = "project" id = "' + project.projectID + '"><h1 style="margin: 0px;" onclick = "sendToProjectView(&apos;' + project.projectID + '&apos;)">' + project.title + '</h1><div>Status: ' + project.status + '</div><div>Due Date: ' + m[1] + "/" + m[2].substring(0,2) + "/" + m[0] + '</div></div><p></p>');
+			});
+		  })
+			.fail( function(d, textStatus, error) {
+				console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+			})
+	  })
+	  .fail( function(d, textStatus, error) {
+		console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+		})
+};
+
+var hourOrSal = function (binary){
+	if (binary == "0"){
+		return "Hourly";
+	}
+	else{
+		return "Salary";
+	}
+};
+
+var loadProfile = function (){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(1);
+		if($("#profileInfo").children().length == 0){
+			$.getJSON('/' + id +'/profile.json', function(js) { //return json of employee tuple from their id
+                  $("#profileInfo").append(
+				  		'<p> Employee ID: ' + js.data[0].empID + '</p><p> Department ID: ' + js.data[0].deptID + ' </p><p> Pay Rate: $' + js.data[0].payrate + '</p><p> Pay Type: ' + hourOrSal(js.data[0].hourOrSal) + ' </p><p> Last Paycheck: ' + js.data[0].datePayed + '</p><ul> Lead for Projects:</ul>');
+               })
+			   	.done(function() {
+				  $.getJSON('/' + id +'/homeLoad.json', function(js) { //return json of array of project tuples where employee id is in employee id list
+					  $.each( js.data, function( id, project){
+						if(project.projectLead == $($("#profile").children()[0]).attr("id")){
+							$("#profileInfo").find("ul").append(
+								'<li id = "' + project.projectID + '" onclick = "sendToProjectView(&apos;' + project.projectID + '&apos;)">' + project.title + '</li>');
+						}
+					  });
+					})
+					.fail( function(d, textStatus, error) {
+						console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+				  })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+		}
+		
+		$("#profileBlock").css("display", "block");
+};
+
+var loadDueToday = function (){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(1);
+		if($("#dueToday").children().length == 0){
+			$.getJSON('/' + id +'/dueToday.json', function(js) { // return json with task tuples that match employee ID and also match the current date.
+					$.each( js.data, function( taskid, task){
+						if($("#dueToday").find("#"+ task.projectID).length == 0){
+							$("#dueToday").append(
+							'<h2 onclick = "sendToProjectView(' + task.projectID + ')" ></h2><ul id = "' + task.projectID + '"></ul>');
+						}
+						$("#dueToday").find("#"+ task.projectID).append(
+						'<li id = "' + task.taskID + '">' + task.title + '</li>');
+					});	
+               })
+			   	.done(function() {
+				  $.getJSON('/' + id +'/homeLoad.json', function(js) {  //return json of array of project tuples where employee id is in employee id list
+					  $.each( js.data, function( id, project){
+						$($("#dueToday").find("#"+ project.projectID).prev()).html(project.title);
+					  });
+					})
+					.fail( function(d, textStatus, error) {
+						console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+				  })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+		}
+		
+		$("#dueTodayBlock").css("display", "block");
+};
+
+var loadMeeting = function () {
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(1);
+		if($("#meetingBlock").children().length == 5){
+			$.getJSON('/' + id + '/meetings.json', function(js) {  //return json of array of all meetings with employees ID inside its empID list ordered from oldest date to newest
+					var today = new Date();
+					$.each( js.data, function( n, meeting){
+						var m = (meeting.date).split('-');
+						var date = new Date(m[0], m[1], m[2].substring(0,2));
+						if(today.getDate() == date.getDate()){
+							$("#meetingsToday").append(
+							'<h3>' + m[2].substring(3) + '</h3><p>' + meeting.projectID + '</p>');
+						}
+						else if(today < date){
+							$("#meetings").append(
+							'<h3>'+ m[1] + "/" + m[2].substring(0,2) + "/" + m[0] + " at "+ m[2].substring(3) + '</h3><p>' + meeting.projectID + '</p>');
+						}
+					});	
+               })
+			   	.done(function() {
+				  $.getJSON('/' + id +'/homeLoad.json', function(js) {  // return json of array of project tuples where employee id is in employee id list
+					  $.each( $("#meetingsToday").children("p") , function( n, div){
+						$.each( js.data , function( n, project){
+							if($(div).html() == project.projectID){
+								$(div).html(project.title);
+							}
+						});
+					  });
+					  $.each( $("#meetings").children("p") , function( n, div){
+						$.each( js.data , function( n, project){
+							if($(div).html() == project.projectID){
+								$(div).html(project.title);
+							}
+						});
+					  });
+					})
+					.fail( function(d, textStatus, error) {
+						console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+				  })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+
+		}
+		
+		$("#meetingBlock").css("display", "block");
+};
+
+var loadFinances = function (){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(1);
+		if($("#finance").children().length == 0){
+			$.getJSON('/' + id +'homeLoad.json', function(js) {  //return json of array of project tuples where employee id is in employee id list
+					$.each( js.data, function( n, project){
+						if($("#finance").find("#"+ project.projectID + "2").length == 0 & project.projectLead == $($("#profile").children()[0]).attr("id")){
+							$("#finance").append(
+							'<h2 onclick = "sendToProjectView(' + project.projectID + '2)" >' + project.title + '</h2><p>Budget: $' + project.budget + '</p><p>Current Balance: $' + project.current_balance + '</p><h3>Last 3 Transactions:</h3><div id = "' + project.projectID + '"></div><button type="submit" onclick = "new function(){$(&quot;#form-' + project.projectID + '&quot;).css(&quot;display&quot;, &quot;block&quot;);}">New Transaction</button><form id = "form-'+ project.projectID +'" style = "display: none;"><h3> New Project Transaction </h3><label for="pID">Project ID:</label><br><input type="text" id="pID" name="pID" value = "'+ project.projectID +'" readonly><br><label for="payID">Employee ID:</label><br><input type="text" id="payID" name="payID" value = "' + $("#profile").children("p").attr("id") + '" readonly><br><label for="amount">Amount: (use negative if withdrawl)</label><br><input type="text" id="amount" name="amount" required><br><label for="desc">Description:</label><br><textarea type="text" id="desc" name="desc" rows = 3 required></textarea><br><label for="dest">Destination: </label><br><input type="text" id="dest" name="dest" required><br><button type="submit">Submit</button></form>');
+						}
+					});	
+               })
+			   	.done(function() {
+				  $.getJSON('/' + id +'json/finances.json', function(js) {  // return last 3 transactions for all projects where employee is a project lead
+					  $.each( js.data, function( id, trans){
+						$("#finance").find("#"+ trans.projectID).append(
+						'<li id = "' + trans.transID + '">' + trans.date + ' - $' + trans.amount + ': ' + trans.description + '</li>');
+					  });
+					})
+					.fail( function(d, textStatus, error) {
+						console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+				  })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+
+		}
+		
+		$("#financeBlock").css("display", "block");
+};
+
+var loadNewAcct = function (){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(1);
+		if($("#newAcct").children().length == 0){
+			$.getJSON('/' + id +'/profile.json', function(js) { //return json of employee tuple from their id 
+                  $("#newAcct").append(
+				  '<form><label for="fname">First name:</label><br><input type="text" id="fname" name="fname" required><br><label for="mname">Middle name:</label><br><input type="text" id="mname" name="mname"><br><label for="lname">Last name:</label><br><input type="text" id="lname" name="lname" required><br><label for="deptID">Department ID:</label><br><input type="text" id="deptID" name="deptID" required><br><label for="payrate">Pay Rate: </label><br><input type="text" id="payrate" name="payrate" required><br><input type="radio" id="salary" name="salOrHour" value="Salary"><label for="Salary" required>Salary</label><br><input type="radio" id="hourly" name="salOrHour" value="Hourly"><label for="Hourly" required>Hourly</label><br><input type="submit" value="Submit"></form>');
+               })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+		}
+		
+		$("#adminBlock").css("display", "block");
+};
+
+var sendToProjectView = function(id){
+	$("#projID").attr("value", id);
+	document.getElementById("openProject").submit();
+};
+
+var hideAll = function (m){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	var divs = $("body").children("div");
+	$.each(divs, function( n, block ){
+		if(n > m){
+			$(block).css("display", "none");
+		}
+	});
+};
+
+var loadProjView = function (){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	$.getJSON('/' + id +'/taskList.json', function(js) { // return json of all task tuples with selected project id
+		  $.each( js.data, function( n, task ){
+			  var m = task.startDate.split('-');
+			$("#" + task.status).append('<div class = "task" id = "' + task.taskID + '"onclick = "loadTask(' + task.taskID+ ')" ><h3>' + task.title + '</h3><p id = "' + task.empID + '"></p><p> Start Date: ' + m[1] + "/" + m[2].substring(0,2) + "/" + m[0] + ' </p><p> Tags: ' + task.tags + ' </p></div>');
+		  });
+	   })
+		.done(function() {
+			$.getJSON('json/projectEmps.json', function(js) { // return json of all employee tuples who work on selected project
+			  $.each( $("#projectBlock").children(".row").children().children("div"), function( n, task ){
+				$.each(js.data, function (m, emp){
+					if($($(task).children()[1]).attr("id") == emp.empID){
+						$($(task).children()[1]).append(emp.firstName + " " + emp.lastName);
+					}
+				});
+			  });
+		   })
+			.done(function() {
+			$.getJSON('/' + id +'/projectData.json', function(js) { // return json of the selected projects tuple
+				$("#overview").html(js.data[0].overview);
+				$("#projectName").html(js.data[0].title);
+				var url = window.location.href.split("/");
+				$("#empID").attr("value", url[url.length-2]);
+		   })
+		   	.fail( function(d, textStatus, error) {
+				console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+			})
+		})
+		   	.fail( function(d, textStatus, error) {
+				console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+			})
+		})
+		.fail( function(d, textStatus, error) {
+			console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+		})
+};
+
+var loadTask = function (div){
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	if($("#taskBlock").css("display") == "none"){
+		$.getJSON('json/taskList.json', function(js) { // return json of all task tuples with selected project id
+				$.each( js.data, function(n, task) {
+					if(task.taskID == $(div).attr("id")){
+						$("#title").attr("value",task.title); 
+						$("#overview2").html(task.overview);
+						$("#tags").attr("value",task.tags);
+						$("#startDate").attr("value",task.startDate);
+						$("#estTime").attr("value",task.estTime);
+						$("#finDate").attr("value",task.finDate);
+						$("#totalTime").attr("value",task.totalTime);
+						$("#taskID").attr("value",task.taskID);
+						$("#" + task.status + "2").attr("checked", true);
+					}
+					
+				})
+		   })
+			  .fail( function(d, textStatus, error) {
+				console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+				})		
+		$("#taskBlock").css("display", "block");
+	}
+	else{
+		$("#taskID").attr("value","");
+		$("#taskBlock").css("display", "none");
+		
+	}
+};
+
+var loadMeetLog = function () {
+	var arr = window.location.href.split("/");
+	var id = arr[arr.length-2];
+	hideAll(0);
+		if($("#meetingLog").children().length == 1){
+			$.getJSON('/meetings2.json', function(js) {  //return json of array of all meetings with project ID, ordered from oldest date to newest
+					var today = new Date();
+					$.each( js.data, function( n, meeting){
+						var m = (meeting.date).split('-');
+						var date = new Date(m[0], m[1], m[2].substring(0,2));
+						if(today > date && !(today.getDate() == date.getDate())){
+							$("#meetingLog").append('<h2> Meeting on ' + m[1] + "/" + m[2].substring(0,2) + "/" + m[0] + " at "+ m[2].substring(3) + '</h2><p>"' + meeting.notes + '"</p>');
+						}
+						else{
+							$("#futureMeetings").append('<h2> Meeting on ' + m[1] + "/" + m[2].substring(0,2) + "/" + m[0] + " at "+ m[2].substring(3) + '</h2>');
+						}
+					});	
+               })
+				  .fail( function(d, textStatus, error) {
+					console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+					})
+
+
+		}
+		
+		$("#meetingBlock").css("display", "block");
+};
+
---
+++ server.js
@@ -0,0 +1,302 @@
+const express = require('express')
+const app = express()
+var mysql = require("mysql");
+const bodyParser = require("body-parser");
+var path = require('path');
+const port = process.env.PORT || 4000;
+app.set('view-engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use('/static', express.static('public'))
+app.use(bodyParser.json());
+var mysqlConnection = mysql.createConnection({
+    host: 'team12servermysql2.mysql.database.azure.com',
+    port: '3306',
+    user: 'team12adminmysql@team12servermysql2',
+    password : 'Team12ProjectManagementMySQL',
+    database : `projectmanagementdb`,
+    ssl: true
+});
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/public/css/" + "stylesheet.css");
+  });
+app.get('/setup.js', function(req, res) {
+    res.sendFile(__dirname + "/public/js/" + "setup.js");
+  });
+app.get('/jquery.min.js', function(req, res) {
+    res.sendFile(__dirname + "/public/js/" + "jquery.min.js");
+  });
+
+
+
+
+mysqlConnection.connect((err)=>{  
+    if(!err)
+    {
+       console.log("Connected");
+    }
+    else{
+      console.log(err);
+    }
+})
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+app.post('/project.html', (req, res)=> {
+    console.log(req.body);
+    res.sendFile(path.join(__dirname + '/public/project.html'));
+})
+
+app.get('/:id/taskList.json', (req, res)=> {
+    var searchID = req.params.id;
+    console.log('its getting task');
+    var sql = 'SELECT taskID, empID, title, overview, startDate, finDate, estTime, totalTime, tags, status FROM projectmanagementdb.task WHERE projectID = ?';
+    mysqlConnection.query(sql, [searchID], function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+    })
+})
+app.get('/projects', (req, res)=> {
+    console.log('/projects')
+    var projectID = req.body;
+    console.log(projectID);
+    mysqlConnection.query('SELECT projectID, projectLead, timeTotal, title, overview, status, startDate, finDate, estTime, budget, current_balance FROM projectmanagementdb.project', projectID, function(err, results, field){
+        if(err) throw err;
+        console.log(results);
+        res.json({data: results});
+    })
+    
+})
+app.get('/userslist', (req,res) =>{
+
+    mysqlConnection.query('SELECT * FROM user', function(err, results, field){
+        if(err) throw err;
+        res.json( {data: results});
+    })
+})
+app.get('/admin/userslist', (req,res) =>{
+
+    mysqlConnection.query('SELECT * FROM projectmanagementdb.employee', function(err, results, field){
+        if(err) throw err;
+        console.log(results);
+        res.render('userslist.ejs');
+    })
+})
+app.post('/userslist', (req, res) => {
+    var email = req.body.email;
+    var sql = "DELETE FROM Employee WHERE email = ?";
+    console.log(req.body);
+    mysqlConnection.query(sql, [email], function(err, results, fields) {
+        
+        if(err){
+            console.log('Could not delete' + req.body.email);
+        }
+        else {
+            console.log('deleted' + email);
+        }
+    })
+})
+app.get('/admin/portal', (req, res)=> {
+    
+    res.render('admin.ejs');
+})
+app.post('/projects', (req, res)=> {
+    
+    console.log(req.body);
+    res.redirect('/projects');
+})
+app.get('/:id/home', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT * FROM projectmanagementdb.employee WHERE empID = ?';
+    mysqlConnection.query(sql, [searchID], function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.render('home.ejs', {data: results});
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+app.get('/:id/tasks', (req, res)=> {
+    var searchID = req.params.id;
+    console.log('its getting task');
+    var sql = 'SELECT taskID, empID, title, overview, startDate, finDate, estTime, totalTime, tags, status FROM projectmanagementdb.Task WHERE empID = ?';
+    mysqlConnection.query(sql, [searchID], function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+    })
+})
+app.get('/:id/projectEmp.json', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT empID, firstName, lastName FROM Employee';
+    mysqlConnection.query(sql, function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+    })
+})
+app.get('/:id/profile.json', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT empID, deptID, firstName, lastName, payrate, hourOrSal, datePayed, empType FROM projectmanagementdb.employee WHERE empID= ?';
+    mysqlConnection.query(sql, searchID, function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+app.get('/:id/dueToday.json', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT taskID, projectID, title FROM projectmanagementdb.Task where';
+    mysqlConnection.query(sql, searchID, function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }else{
+            console.log(err);
+        }
+    })
+})
+app.get('/:id/homeLoad.json', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT projectID, projectLead, title, status, finDate, budget, current_balance FROM projectmanagementdb.project WHERE projectLead = ?';
+    mysqlConnection.query(sql,searchID, function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+
+app.get('/:id/finances.json', (req, res)=> {
+    var searchID = req.params.id;
+    var sql = 'SELECT transID, projectID, date, amount, description FROM projectmanagementdb.transaction WHERE empID= ?';
+    mysqlConnection.query(sql, searchID, function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }else{
+            console.log(err);
+        }
+    })
+})
+app.get('/:id/meetings.json', (req, res)=> {
+    var searchID = req.params.id;
+
+    mysqlConnection.query('SELECT meetings.meetingID, meetings.projectID, date, notes FROM projectmanagementdb.meetings LEFT JOIN projectmanagementdb.meeting_relation on meeting_relation.meetingID = meetings.meetingID WHERE meeting_relation.empID = ? ORDER by meetings.date asc limit 2', [searchID], function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+app.get('/meetings2.json', (req, res)=> {
+   
+
+    mysqlConnection.query('SELECT meetingID, projectID, date, notes FROM projectmanagementdb.meetings', function(err, results, fields){
+        if(!err){
+            console.log(results);
+            res.json({data: results});
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+app.post('/login', (req, res)=>{
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log(req.body);
+
+    mysqlConnection.query('SELECT * FROM projectmanagementdb.employee WHERE email = ?', [email],function(err, results, fields){
+        if(err)
+        {
+            console.log("email could not be found", err);
+            res.end({
+                "code" : 400,
+                "failed" : "error occured"
+            })
+        }
+        else{
+            
+            if(results.length>0)
+            {
+                
+                if(results[0].password == password){
+                    console.log("logging in")
+                    if(results[0].empID == 1)
+                    {
+                        res.redirect('/admin/portal');
+                    }
+                    else{
+                        res.redirect('/' + results[0].empID + '/home');
+                    }
+                }
+                else{
+                    console.log("password did not match")
+                    res.send({
+                        "code": 204,
+                        "success" : "email and password does not match"
+                    })
+                }
+
+            }
+            
+        }
+    } )
+})
+
+
+
+app.get('/register', (req, res)=> {
+    console.log('wtf');
+    res.render('register.ejs');
+})
+
+app.post('/register', (req, res)=>{
+    console.log(req.body);
+    var id = Math.round(Math.random() * 999999) + 100000;
+    console.log(id);
+    sql = "INSERT INTO Employee SET ?";
+    var values = {
+        empID: id,
+        email: req.body.email,
+        password:  id + req.body.lastName,
+        deptID: '1',
+        firstName: req.body.firstName,
+        midName: req.body.midName,
+        lastName: req.body.lastName,
+        payrate: req.body.payrate,
+        hourOrSal: req.body.hourOrSal
+    };
+    
+    mysqlConnection.query(sql, values, function(err,result){
+        if(!err){
+            console.log('added');
+            console.log(values);
+            res.redirect('/');
+        }
+        else{
+            console.log(err);
+            res.redirect('/register');
+            console.log('did not add to user table');
+        }
+    })
+    
+    
+})
+app.listen(port);
