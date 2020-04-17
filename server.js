64aac531e3e06e69b8aa114b951786fdf96b2e43
jmhorn2015 <jmhorn2015@gmail.com>
2020-04-17 16:29:22 -0500

    finish report forms

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ const options = { transports: ['websocket'], pingTimeout: 3000, pingInterval: 50
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 })
+app.get('/report', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/public/report.html'));
+})
 app.post('/project.html', (req, res)=> {
     var projID = req.body.projID;
     res.redirect('/' + projID + '/project');
